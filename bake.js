6e00119842c6212d376a4da272679581aeda041e
Eric Muyser <eric@muyser.com>
2011-06-22 22:41:09 -0700

    tidy up baker.

--- bake.js
+++ bake.js
@@ -7,37 +7,37 @@ var exec = function(command, params) {
 	var subprocess = spawn(command, params);
 	
 	subprocess.stdout.on('data', function(data) {
-	  sys.print(data.asciiSlice(0, data.length));
+		sys.print(data.asciiSlice(0, data.length));
 	});
 	
 	subprocess.stderr.on('data', function(data) {
-	  sys.print(data.asciiSlice(0, data.length));
+		sys.print(data.asciiSlice(0, data.length));
 	});
 };
 
 
 var baker = function(input_path, output_path) {
 	stalker.watch(input_path, function(err, file_path) {
-	  if(err) {
-	    console.log('Error: ' + err);
-	    
-	    return;
-	  }
-	  
-	  var extension = file_path.split('.').pop();
-	  
-	  if(extension !== 'less')
-	  	return;
-	  	
-	  var compile = function() {
-		  var new_file_path = file_path.replace(input_path, output_path).replace(/^.*\\/, '').replace(/\.[^\.]*$/, '.css');
+		if(err) {
+			console.log('Error: ' + err);
+			
+			return;
+		}
+		
+		var extension = file_path.split('.').pop();
+		
+		if(extension !== 'less')
+			return;
+			
+		var compile = function() {
+			var new_file_path = file_path.replace(input_path, output_path).replace(/^.*\\/, '').replace(/\.[^\.]*$/, '.css');
 			console.log('less - compiled ' + new_file_path);
-		  exec('lessc', [file_path, new_file_path]);
-	  };
-	  
-	  compile();
-	  
-	  fs.watchFile(file_path, function(curr, prev) {
+			exec('lessc', [file_path, new_file_path]);
+		};
+		
+		compile();
+		
+		fs.watchFile(file_path, function(curr, prev) {
 			compile();
 		});
 	});
